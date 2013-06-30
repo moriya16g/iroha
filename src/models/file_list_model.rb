995b6e1853702eda9b742021ab71f4d94251bce8
Shigeru Moriya <moriya@sarotti.net>
2013-06-30 23:48:31 +0900

    add a FileListModel class. add a FileModel class.

---
+++ src/models/file_list_model.rb
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+require File.expand_path(File.dirname(__FILE__) + '/file_model')
+
+# ファイルリストを扱うクラス
+# Authors :: Shigeru Moriya <moriya@sarotti.net>
+class FileListModel
+  
+  # ファイルリストを取得するディレクトリ
+  @path = nil
+  # ファイルリスト
+  @list = []
+
+  # コンストラクタ
+  # ==== args
+  # path :: ファイルリストを取得するディレクトリ
+  def initialize(path)
+    @path = path
+    list_up()
+  end
+
+  # ファイル名順でファイルリストをソートする
+  # ==== args
+  # dir :: ディレクトリを特別扱いするか?
+  # reverse :: 逆順にソートするか?
+  def sort_in_filename(dir = true, reverse = false)
+    dir_list = []
+    if dir
+      dir_list = get_dirs
+      dir_list = dir_list.sort{|a, b| a.filename <=> b.filename}
+      file_list = get_files
+    else
+      file_list = @list.clone
+    end
+    file_list = file_list.sort{|a, b| a.filename <=> b.filename}
+    if reverse
+      @list = dir_list.reverse + file_list.reverse
+    else
+      @list = dir_list + file_list
+    end
+  end
+
+  # 拡張子順でファイルリストをソートする
+  # ==== args
+  # dir :: ディレクトリを特別扱いするか?
+  # reverse :: 逆順にソートするか?
+  def sort_in_extention(dir = true, reverse = false)
+    dir_list = []
+    if dir
+      dir_list = get_dirs
+      dir_list = dir_list.sort{|a, b| a.filename <=> b.filename}
+      file_list = get_files
+    else
+      file_list = @list.clone
+    end
+    file_list = file_list.sort{|a, b| 
+      if a.extention.empty? and b.extention.empty?
+        a.filename <=> b.filename
+      elsif a.extention.empty? and b.extention.empty? == false
+        -1
+      elsif a.extention.empty? == false and b.extention.empty?
+        1
+      else
+        a.extention <=> b.extention
+      end
+    }
+    if reverse
+      @list = dir_list.reverse + file_list.reverse
+    else
+      @list = dir_list + file_list
+    end
+  end
+ 
+  # サイズ順でファイルリストをソートする
+  # ==== args
+  # dir :: ディレクトリを特別扱いするか?
+  # reverse :: 逆順にソートするか?
+  def sort_in_size(dir = true, reverse = false)
+    dir_list = []
+    if dir
+      dir_list = get_dirs
+      dir_list = dir_list.sort{|a, b| a.size <=> b.size}
+      file_list = get_files
+    else
+      file_list = @list.clone
+    end
+    file_list = file_list.sort{|a, b| a.size <=> b.size}
+    if reverse
+      @list = dir_list.reverse + file_list.reverse
+    else
+      @list = dir_list + file_list
+    end
+  end
+
+  # タイムスタンプ順でファイルリストをソートする
+  # ==== args
+  # dir :: ディレクトリを特別扱いするか?
+  # reverse :: 逆順にソートするか?
+  def sort_in_timestamp(dir = true, reverse = false)
+    dir_list = []
+    if dir
+      dir_list = get_dirs
+      dir_list = dir_list.sort{|a, b| a.timestamp <=> b.timestamp}
+      file_list = get_files
+    else
+      file_list = @list.clone
+    end
+    file_list = file_list.sort{|a, b| a.timestamp <=> b.timestamp}
+    if reverse
+      @list = dir_list.reverse + file_list.reverse
+    else
+      @list = dir_list + file_list
+    end
+  end
+
+  def print
+    @list.size.times{|i|
+      p @list[i]
+    }
+  end
+  
+  # ファイルリストを取得・更新する
+  def list_up
+    @list = []
+    Dir.glob(@path + '/.*').each{|f|
+      @list << FileModel.new(f)
+    }
+    Dir.glob(@path + '/*').each{|f|
+      @list << FileModel.new(f)
+    }
+  end
+  private :list_up
+  
+  # ファイルリストからディレクトリのみを抽出
+  # ==== return
+  # ディレクトリのみのリスト
+  def get_dirs
+    return @list.select{|f| f.is_dir == true}
+  end
+  private :get_dirs
+  
+  # ファイルリストからファイルのみを抽出
+  # ==== return
+  # ファイルのみのリスト
+  def get_files
+    return @list.select{|f| f.is_dir == false}
+  end
+  private :get_files
+end
---
+++ src/models/file_model.rb
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+# ファイルリストを扱うクラス
+class FileModel
+  # ディレクトリかどうか
+  @is_dir
+  # ファイル・ディレクトリ名
+  @filename
+  # 拡張子
+  @extention
+  # サイズ
+  @size
+  # タイムスタンプ
+  @timestamp
+
+  attr_reader :is_dir, :filename, :extention, :size, :timestamp
+  
+  # コンストラクタ
+  # ==== args
+  # f :: ファイル名
+  def initialize(f)
+    @is_dir = is_dir(f)
+    @timestamp = get_timestamp(f)
+    if @is_dir
+      @filename = get_dirname(f)
+      @extention = ""
+      @size = 0
+    else
+      @filename = get_filename(f)
+      @extention = get_extention(f)
+      @size = get_size(f)
+    end
+  end
+
+  # ディレクトリかどうか
+  # ==== args
+  # f :: 調べるファイル・ディレクトリ
+  # ==== return
+  # true :: ディレクトリである
+  # false :: ファイルである
+  def is_dir(f)
+    return File::stat(f).directory?
+  end
+  private :is_dir
+
+  # ファイル名を取得
+  # ==== args
+  # f :: 調べるファイル
+  # ==== return
+  # ファイル
+  def get_filename(f)
+    return File::basename(f, '.*')
+  end
+  private :get_filename
+
+  # ディレクトリ名を取得
+  # ==== args
+  # d :: 調べるディレクトリ
+  # ==== return
+  # ディレクトリ名
+  def get_dirname(d)
+    return File::basename(d)
+  end
+  private :get_dirname
+  
+  # 拡張子を取得
+  # ==== args
+  # f :: 調べるファイル
+  # ==== return
+  # 拡張子
+  def get_extention(f)
+    return File::extname(f)
+  end
+  private :get_extention
+
+  # ファイルサイズを取得
+  # ==== args
+  # f :: 調べるファイル
+  # ==== return
+  # ファイルサイズ
+  def get_size(f)
+    return File::stat(f).size()
+  end
+  private :get_size
+
+  def get_timestamp(f)
+    return File::stat(f).mtime()
+  end
+end
