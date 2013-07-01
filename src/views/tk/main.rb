c74919f927073f42439a476812256e6ee4c803b0
Shigeru Moriya <moriya@sarotti.net>
2013-07-02 02:21:01 +0900

    add MainWindowView class as a sample.

---
+++ src/views/tk/main.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+require 'tk'
+
+# メインウィンドウを扱うクラス
+# Author :: Shigeru Moriya <moriya@sarotti.net>
+class MainWindowView
+  
+  # コンストラクタ
+  # ==== args
+  # title :: メインウィンドウのタイトル
+  # size_x :: メインウィンドウのx方向サイズ
+  # size_y :: メインウィンドウのy方向サイズ
+  # pos_x :: メインウィンドウのx方向位置
+  # pos_y :: メインウィンドウのy方向位置
+  def initialize(title = 'iroha', 
+                 size_x = 800, size_y = 600, 
+                 pos_x = 200, pos_y = 100)
+    root = TkRoot.new {
+      title title
+      #width size_x
+      #height size_y
+      minsize 0, 0
+      #maxsize size_x, size_y
+      resizable 1, 1
+      geometry size_x.to_s + 'x' + size_y.to_s + 
+        '+' + pos_x.to_s + '+' + pos_y.to_s
+    }
+    create_double_window(root)
+  end
+  
+  # 二画面ウィンドウを生成
+  # ==== 
+  def create_double_window(parent)
+    paned1 = TkPanedWindow.new(parent, :orient=>:vertical) {
+      paned2 = TkPanedWindow.new(paned1, :orient=>:horizontal) {
+        @left = TkCanvas.new(paned2, :bg=>'black')
+        @right = TkCanvas.new(paned2, :bg=>'black')
+        add @left, @right
+        pack(:fill=>:both, :expand=>true, :side=>:top)
+      }
+      @buttom = TkCanvas.new(paned1, :bg=>'black')
+      add paned2, @buttom
+      pack(:fill=>:both, :expand=>true, :side=>:top)
+    }
+  end
+  
+end
+
+
+iroha = MainWindowView.new
+Tk.mainloop
+
