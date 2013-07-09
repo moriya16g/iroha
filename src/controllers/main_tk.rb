c4ee6acdb4327585053f7bbfd696f84d2ff4f516
Shigeru Moriya <moriya@sarotti.net>
2013-07-10 01:52:29 +0900

    rename main.rb to main_window_view.rb.

--- src/controllers/main_tk.rb
+++ src/controllers/main_tk.rb
@@ -3,7 +3,7 @@
 require 'tk'
 require File.expand_path(File.dirname(__FILE__) + '/../models/config_model')
 require File.expand_path(File.dirname(__FILE__) + '/../models/file_list_model')
-require File.expand_path(File.dirname(__FILE__) + '/../views/tk/main')
+require File.expand_path(File.dirname(__FILE__) + '/../views/tk/main_window_view')
 require File.expand_path(File.dirname(__FILE__) + '/../views/tk/filer_view')
 
 f1 = FileListModel.new("c:/")
